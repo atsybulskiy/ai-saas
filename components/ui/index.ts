4a4493a2983c91056d9d2bab881181ca7f623717
atsybulskiy <atsibulskiy@gmail.com>
2023-07-27 14:06:02 +0300

    Image Generator

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,24 @@
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
 
-export { Input, Button, Form, FormControl, FormField, FormItem };
+export {
+  Input,
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue
+};
